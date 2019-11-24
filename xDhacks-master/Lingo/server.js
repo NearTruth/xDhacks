ac2850a7e444a0aad032f767c612fccf5bcc6a6a
NearTruth <55720636+NearTruth@users.noreply.github.com>
2019-11-23 21:46:06 -0800

    Update server.js

--- xDhacks-master/Lingo/server.js
+++ xDhacks-master/Lingo/server.js
@@ -4,27 +4,27 @@ var fs = require('fs');
 var events = require('events');
 
 http.createServer(function(req, res){
-if(req.url === '/favicon.ico'){res.end();}
-else{
-res.writeHead(200, {'Content-Type': 'test/html'});
-var q = url.parse(req.url, true).query;
-var n = q.name;
-var l1 = q.firstLanguage;
-var l2 = q.secondLanguage;
+    if(req.url === '/favicon.ico'){res.end();}
+    else{
+        res.writeHead(200, {'Content-Type': 'test/html'});
+        var q = url.parse(req.url, true).query;
+        var n = q.name;
+        var l1 = q.firstLanguage;
+        var l2 = q.secondLanguage;
 
-console.log(n+" "+l1+" "+l2+"\n");
-console.log(req.url);
+        console.log(n+" "+l1+" "+l2+"\n");
+        console.log(req.url);
 
-fs.readFile('./main.html', function(err, data) {
-if (err) {
-res.writeHead(404, {'Content-Type': 'text/html'});
-return res.end("404 Not Found");
-}
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.write(data);
-return res.end();
-});
-}
+        fs.readFile('./main.html', function(err, data) {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end("404 Not Found");
+            }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+        });
+    }
 }).listen(8080);
 
 
